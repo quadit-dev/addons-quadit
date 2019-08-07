9c75b511d404a0edd011f4d241b0943de76a4e0f
GabrielSnader <gabriel.lopez@quadit.mx>
2019-08-07 18:01:26 -0500

    report_stock

---
+++ report_stock_picking/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Reporte de Inventario',
+    'version': '12.0.1.0.0',
+    'summary': '',
+    'description': "",
+    'website': '',
+    'depends': ['stock'],
+    'category': 'Inventary',
+    'sequence': 13,
+    'demo': [
+    ],
+    'data': [
+        'report/stock_report.xml',
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
